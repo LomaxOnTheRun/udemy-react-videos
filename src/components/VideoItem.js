e4b1c5292b25ad53021c02512ccba7b10dacb573
LomaxOnTheRun <lomax.on.the.run@gmail.com>
2018-12-30 16:31:12 +1100

    Add image to VideoItem

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const VideoItem = props => {
-  return <div>Video item</div>;
+const VideoItem = ({ video }) => {
+  console.log(video);
+  return (
+    <div>
+      <img
+        alt={video.snippet.description}
+        src={video.snippet.thumbnails.medium.url}
+      />
+      {video.snippet.title}
+    </div>
+  );
 };
 
 export default VideoItem;
